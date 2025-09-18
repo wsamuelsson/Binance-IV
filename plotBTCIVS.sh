eb4831e9b07f72a1ffbe6c4d0db3de8bbc64bbee
WUTABUTNO <wilsam123@hotmail.se>
2025-09-18 10:20:02 +0200

    Puts included

--- plotBTCIVS.sh
+++ plotBTCIVS.sh
@@ -2,19 +2,20 @@ write_option_script="write_option_data.py"
 
 symbol_flag="--symbol=btc" 
 side_flag="--side=ask" 
-type_flag="--type=c" 
+type_flag_call="--type=c" 
+type_flag_put="--type=p"
 
 plot_python_script="plot_out.py"
 
 
-python3 "$write_option_script" "$symbol_flag" "$side_flag" "$type_flag"
-
+python3 "$write_option_script" "$symbol_flag" "$side_flag" "$type_flag_call"
+python3 "$write_option_script" "$symbol_flag" "$side_flag" "$type_flag_put"
 make
 
+./compute_IV b a p
 ./compute_IV b a c
 
 python3 "$plot_python_script"
 
-make clean
 
 
